5bec5fb1a3dc9ba80d9c2720014f17016338725c
omelia86 <gulnaz.umitbaeva@gmail.com>
2020-10-07 12:49:44 +0300

    Added Tutorial1FirstApp

---
+++ MetanitCSharp/Tutorial1FirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator.Add(2, 3);
+            Console.ReadKey();
+        }
+    }
+    class Calculator
+    {
+        public static void Add(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine($"Сумма {x} и {y} равна {z}");
+
+        }
+    }
+}
